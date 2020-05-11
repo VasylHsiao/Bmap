9f737d2a60b5ea99569af36a3e869e83b2625f6e
VasylHsiao <61651192+VasylHsiao@users.noreply.github.com>
2020-05-11 13:53:42 +0800

    use demo 1

--- app/src/main/java/com/example/bmap1_map/MainActivity.java
+++ app/src/main/java/com/example/bmap1_map/MainActivity.java
@@ -2,106 +2,306 @@ package com.example.bmap1_map;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
 import com.baidu.location.Poi;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapView;
+import com.baidu.location.PoiRegion;
+import com.example.bmap1_map.service.LocationService;
+import com.example.bmap1_map.service.Utils;
+
+//public class MainActivity extends Activity {
+//    private MapView mMapView = null;
+//    BaiduMap mMap;
+//    public LocationClient mLocationClient = null;
+//    private MyLocationListener myListener = null;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        //获取地图控件引用
+//        mMapView = (MapView) findViewById(R.id.bmapView);
+//        mMap = mMapView.getMap();
+//        mMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+//
+//        mMap.setMyLocationEnabled(true);//开启定位图层
+//
+//        //初始化LocationClient类
+//        mLocationClient = new LocationClient(getApplicationContext());
+//        myListener = new MyLocationListener();
+//        mLocationClient.registerLocationListener(myListener);//注册监听函数
+//        //配置参数
+//        LocationClientOption option = new LocationClientOption();
+//        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，设置定位模式，默认高精度
+//        option.setCoorType("bd09ll");
+//        option.setScanSpan(1000);
+//        option.setOpenGps(true);
+//        option.setLocationNotify(true);
+//        option.setIgnoreKillProcess(false);//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+//        option.setIsNeedAddress(true);//可选，是否需要地址信息，默认为不需要，即参数为false
+//        option.setNeedNewVersionRgc(true);
+//        option.setIsNeedLocationDescribe(true);
+//        mLocationClient.setLocOption(option);//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+//
+//        mLocationClient.start();//调用LocationClient的start()方法，便可发起定位请求
+//
+//
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+//        mMapView.onResume();
+//        mLocationClient.restart();//调用LocationClient的start()方法，便可发起定位请求
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+//        mMapView.onPause();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+//        mMapView.onDestroy();
+//    }
+//
+//    public class MyLocationListener extends BDAbstractLocationListener {
+//        @Override
+//        public void onReceiveLocation(BDLocation location) {
+//            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+//            //以下只列举部分获取经纬度相关（常用）的结果信息
+//            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+//
+//            double latitude = location.getLatitude();    //获取纬度信息
+//            double longitude = location.getLongitude();    //获取经度信息
+//            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+//            String addr = location.getAddrStr();    //获取详细地址信息
+//            String country = location.getCountry();    //获取国家
+//            String province = location.getProvince();    //获取省份
+//            String city = location.getCity();    //获取城市
+//            String district = location.getDistrict();    //获取区县
+//            String street = location.getStreet();    //获取街道信息
+//            String adcode = location.getAdCode();    //获取adcode
+//            String town = location.getTown();    //获取乡镇信息
+//            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+//            String coorType = location.getCoorType();
+//            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+//
+//            int errorCode = location.getLocType();
+//            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+//
+//            System.out.println(latitude + "\n" + longitude);
+//            System.out.println("-----------------");
+//            System.out.println(locationDescribe);
+//            System.out.println("-----------------");
+//            System.out.println(errorCode);
+//        }
+//    }
+//}
 
 public class MainActivity extends Activity {
-    private MapView mMapView = null;
-    BaiduMap mMap;
-    public LocationClient mLocationClient = null;
-    private MyLocationListener myListener = null;
+
+    private LocationService locationService;
+    private TextView LocationResult;
+    private TextView LocationDiagnostic;
+    private Button startLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.bmapView);
-        mMap = mMapView.getMap();
-        mMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
 
-        mMap.setMyLocationEnabled(true);//开启定位图层
-
-        //初始化LocationClient类
-        mLocationClient = new LocationClient(getApplicationContext());
-        myListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myListener);//注册监听函数
-        //配置参数
-        LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，设置定位模式，默认高精度
-        option.setCoorType("bd09ll");
-        option.setScanSpan(1000);
-        option.setOpenGps(true);
-        option.setLocationNotify(true);
-        option.setIgnoreKillProcess(false);//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
-        option.setIsNeedAddress(true);//可选，是否需要地址信息，默认为不需要，即参数为false
-        option.setNeedNewVersionRgc(true);
-        option.setIsNeedLocationDescribe(true);
-        mLocationClient.setLocOption(option);//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
-
-        mLocationClient.start();//调用LocationClient的start()方法，便可发起定位请求
-
-
+        // -----------demo view config ------------
+        setContentView(R.layout.location);
+        LocationResult = (TextView) findViewById(R.id.textView1);
+        LocationDiagnostic = (TextView) findViewById(R.id.textView2);
+        LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
+        startLocation = (Button) findViewById(R.id.addfence);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-        mLocationClient.restart();//调用LocationClient的start()方法，便可发起定位请求
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        locationService.unregisterListener(mListener); //注销掉监听
+        locationService.stop(); //停止定位服务
+        super.onStop();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
+    protected void onStart() {
+        super.onStart();
+
+        // TODO Auto-generated method stub
+        // -----------location config ------------
+        //获取locationservice实例
+        locationService = ((Map) getApplication()).locationService;
+        //注册监听
+        locationService.registerListener(mListener);
+        int type = getIntent().getIntExtra("from", 0);
+        if (type == 0) {
+            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+        } else if (type == 1) {
+            locationService.start();
+        }
+        startLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
+                    locationService.start();// 定位SDK
+                    // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
+                    startLocation.setText(getString(R.string.stoplocation));
+                } else {
+                    locationService.stop();
+                    startLocation.setText(getString(R.string.startlocation));
+                }
+            }
+        });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-    }
+    private BDAbstractLocationListener mListener = new BDAbstractLocationListener() {
 
-    public class MyLocationListener extends BDAbstractLocationListener {
         @Override
         public void onReceiveLocation(BDLocation location) {
-            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-            //以下只列举部分获取经纬度相关（常用）的结果信息
-            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                int tag = 1;
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
+                 */
+                sb.append(location.getTime());
+                sb.append("\nlocType : ");// 定位类型
+                sb.append(location.getLocType());
+                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+                sb.append(location.getLocTypeDescription());
+                sb.append("\nlatitude : ");// 纬度
+                sb.append(location.getLatitude());
+                sb.append("\nlongtitude : ");// 经度
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");// 半径
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");// 国家码
+                sb.append(location.getCountryCode());
+                sb.append("\nProvince : ");// 获取省份
+                sb.append(location.getProvince());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+                sb.append("\nTown : ");// 获取镇信息
+                sb.append(location.getTown());
+                sb.append("\nStreet : ");// 街道
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");// 地址信息
+                sb.append(location.getAddrStr());
+                sb.append("\nStreetNumber : ");// 获取街道号码
+                sb.append(location.getStreetNumber());
+                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+                sb.append(location.getUserIndoorState());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());// 方向
+                sb.append("\nlocationdescribe: ");
+                sb.append(location.getLocationDescribe());// 位置语义化信息
+                sb.append("\nPoi: ");// POI信息
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append("poiName:");
+                        sb.append(poi.getName() + ", ");
+                        sb.append("poiTag:");
+                        sb.append(poi.getTags() + "\n");
+                    }
+                }
+                if (location.getPoiRegion() != null) {
+                    sb.append("PoiRegion: ");// 返回定位位置相对poi的位置关系，仅在开发者设置需要POI信息时才会返回，在网络不通或无法获取时有可能返回null
+                    PoiRegion poiRegion = location.getPoiRegion();
+                    sb.append("DerectionDesc:"); // 获取POIREGION的位置关系，ex:"内"
+                    sb.append(poiRegion.getDerectionDesc() + "; ");
+                    sb.append("Name:"); // 获取POIREGION的名字字符串
+                    sb.append(poiRegion.getName() + "; ");
+                    sb.append("Tags:"); // 获取POIREGION的类型
+                    sb.append(poiRegion.getTags() + "; ");
+                    sb.append("\nSDK版本: ");
+                }
+                sb.append(locationService.getSDKVersion()); // 获取SDK版本
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 速度 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());// 卫星数目
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 海拔高度 单位：米
+                    sb.append("\ngps status : ");
+                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                logMsg(sb.toString(), tag);
+            }
+        }
+    };
 
-            double latitude = location.getLatitude();    //获取纬度信息
-            double longitude = location.getLongitude();    //获取经度信息
-            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
-            String addr = location.getAddrStr();    //获取详细地址信息
-            String country = location.getCountry();    //获取国家
-            String province = location.getProvince();    //获取省份
-            String city = location.getCity();    //获取城市
-            String district = location.getDistrict();    //获取区县
-            String street = location.getStreet();    //获取街道信息
-            String adcode = location.getAdCode();    //获取adcode
-            String town = location.getTown();    //获取乡镇信息
-            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
-            String coorType = location.getCoorType();
-            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+    public void logMsg(final String str, final int tag) {
 
-            int errorCode = location.getLocType();
-            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-
-            System.out.println(latitude + "\n" + longitude);
-            System.out.println("-----------------");
-            System.out.println(locationDescribe);
-            System.out.println("-----------------");
-            System.out.println(errorCode);
+        try {
+            if (LocationResult != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationResult.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (tag == Utils.RECEIVE_TAG) {
+                                    LocationResult.setText(str);
+                                } else if (tag == Utils.DIAGNOSTIC_TAG) {
+                                    LocationDiagnostic.setText(str);
+                                }
+                            }
+                        });
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
