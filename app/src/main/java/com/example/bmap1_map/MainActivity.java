a05a7941a789a64e1dab700845736fc97107e875
VasylHsiao <61651192+VasylHsiao@users.noreply.github.com>
2020-05-11 20:10:50 +0800

    定位调用参数调整完毕

--- app/src/main/java/com/example/bmap1_map/MainActivity.java
+++ app/src/main/java/com/example/bmap1_map/MainActivity.java
@@ -2,31 +2,44 @@ package com.example.bmap1_map;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-import com.baidu.location.Poi;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends Activity {
     private MapView mMapView = null;
     BaiduMap mMap;
     public LocationClient mLocationClient = null;
     private MyLocationListener myListener = null;
+    double mCurrentLat;
+    double mCurrentLon;
+    float mCurrentAccracy;
+    float mCurrentDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
         mMap = mMapView.getMap();
         mMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-
         mMap.setMyLocationEnabled(true);//开启定位图层
+        View child = mMapView.getChildAt(1);
+        if (child != null) {
+            child.setVisibility(View.INVISIBLE);
+        }
 
         //初始化LocationClient类
         mLocationClient = new LocationClient(getApplicationContext());
@@ -79,29 +92,53 @@ public class MainActivity extends Activity {
             //以下只列举部分获取经纬度相关（常用）的结果信息
             //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
 
-            double latitude = location.getLatitude();    //获取纬度信息
-            double longitude = location.getLongitude();    //获取经度信息
-            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
-            String addr = location.getAddrStr();    //获取详细地址信息
-            String country = location.getCountry();    //获取国家
-            String province = location.getProvince();    //获取省份
-            String city = location.getCity();    //获取城市
-            String district = location.getDistrict();    //获取区县
-            String street = location.getStreet();    //获取街道信息
-            String adcode = location.getAdCode();    //获取adcode
-            String town = location.getTown();    //获取乡镇信息
-            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
-            String coorType = location.getCoorType();
-            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+//            double latitude = location.getLatitude();    //获取纬度信息
+//            double longitude = location.getLongitude();    //获取经度信息
+//            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+//            String addr = location.getAddrStr();    //获取详细地址信息
+//            String country = location.getCountry();    //获取国家
+//            String province = location.getProvince();    //获取省份
+//            String city = location.getCity();    //获取城市
+//            String district = location.getDistrict();    //获取区县
+//            String street = location.getStreet();    //获取街道信息
+//            String adcode = location.getAdCode();    //获取adcode
+//            String town = location.getTown();    //获取乡镇信息
+//            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+//            String coorType = location.getCoorType();
+//            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+//
+//            int errorCode = location.getLocType();
+//            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
 
-            int errorCode = location.getLocType();
-            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
 
-            System.out.println(latitude + "\n" + longitude);
-            System.out.println("-----------------");
-            System.out.println(locationDescribe);
-            System.out.println("-----------------");
-            System.out.println(errorCode);
+            //构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection())
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            //设置定位数据，显示定位蓝点
+            mMap.setMyLocationData(locData);
+
+            //构造地理坐标数据
+            LatLng ll = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+            //（以动画方式）改变地图状态（中心、倍数等）
+            MapStatus.Builder builder = new MapStatus.Builder();
+            builder.target(ll).zoom(17.0f);
+            mMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+            mCurrentLat = location.getLatitude();
+            mCurrentLon = location.getLongitude();
+            mCurrentAccracy = location.getRadius();
+            mCurrentDirection = location.getDirection();
+            Log.i("tag", "lat:" + mCurrentLat + "\n" +
+                    "lon:" + mCurrentLon + "\n" + "Radius:" + mCurrentAccracy + "\n" + "Direc:" + mCurrentDirection);
         }
     }
 }
